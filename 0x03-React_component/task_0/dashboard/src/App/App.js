2886336d4e7fe27ad024d8bb67efa8e1043dc884
hassanpc <hass4all.com>
2024-05-29 19:56:37 +0000

    .

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,52 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+//import logo from '../assets/holberton_logo.jpg';
+//import { getFooterCopy, getFullYear } from '../utils/utils';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
-import Notifications from '../Notifications/Notifications';
-import './App.css';
+import Notifications from '../Notifications/Notifications'
 import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
-class App extends React.Component {
-	listCourses = [
-		{ id: 1, name: 'ES6', credit: 60 },
-		{ id: 2, name: 'Webpack', credit: 20 },
-		{ id: 3, name: 'React', credit: 40 },
-	];
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-	listNotifications = [
-		{ id: 1, type: 'default', value: 'New course available' },
-		{ id: 2, type: 'urgent', value: 'New resume available' },
-		{ id: 3, type: 'urgent', html: getLatestNotification() },
-	];
+    this.state = {
+      isLoggedIn: props.isLoggedIn,
+      listCourses: [
+        {id:1, name:'ES6', credit: 60},
+        {id:2, name:'Webpack', credit: 20},
+        {id: 3, name: 'React', credit: 40}
+      ],
+      listNotifications: [
+        {id: 1, value: "New course available", type: "default"},
+        {id: 2, value: "New resume available", type: "urgent"},
+        {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
+      ],
+    };
+  }
 
-	render() {
-		return (
-			<React.Fragment>
-				<div className='App'>
-					<div className='heading-section'>
-						<Notifications listNotifications={this.listNotifications} />
-						<Header />
-					</div>
-					{this.props.isLoggedIn ? (
-						<CourseList listCourses={this.listCourses} />
-					) : (
-						<Login />
-					)}
-					<Footer />
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    // Destructuring in React
+    // Eg Before destructuring: <Notifications listNotifications={this.state.listNotifications}/>
+    const {listNotifications, isLoggedIn, listCourses} = this.state;
+    return(
+      <>
+        <Notifications listNotifications={listNotifications}/>
+        <div className='App'>
+          <Header />
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </>
+    )
+  }
 }
 
 App.defaultProps = {
-	isLoggedIn: false,
+  isLoggedIn: false
 };
 
 App.propTypes = {
-	isLoggedIn: PropTypes.bool,
-};
+  isLoggedIn: PropTypes.bool,
+}
 
 export default App;
